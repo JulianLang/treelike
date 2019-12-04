2290f08fd53c3903db5b1da56d9b9b427f2e1783
Julian Lang <24634068+JulianLang@users.noreply.github.com>
2019-12-04 14:11:17 +0100

    docs: add docs for leaf treverser

--- src/core/traversers/leaf.traverser.ts
+++ src/core/traversers/leaf.traverser.ts
@@ -1,6 +1,17 @@
 import { isDefined } from '../shared';
 import { ObjectTreeNode, TraverseCallbackFn } from '../types';
-
+/**
+ * Traverses a tree leafwise and climbs it recursively up:
+ * ~~~
+ *       (6)
+ *      /   \
+ *    (3)    (5)
+ *    /  \     \
+ *  (1)  (2)    (4)
+ * ~~~
+ * @param node The node to start traversing from.
+ * @param onNext The callback to call for each node being traversed.
+ */
 export function leafTraverser(node: ObjectTreeNode, onNext: TraverseCallbackFn): void {
   for (const child of node.children) {
     leafTraverser(child, onNext);
